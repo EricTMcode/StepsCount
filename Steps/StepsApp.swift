ce48fa33c419635cd759e5907026ed3212c4b098
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-17 15:20:12 +0200

    Initial Commit

---
+++ Steps/StepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsApp.swift
+//  Steps
+//
+//  Created by Eric on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct StepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
